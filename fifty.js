5a54d02f48eae5752dfa0653adf7c79e7ee3d862
in-roma <romain.grandjean@outlook.com>
2020-12-16 18:42:58 +0100

    first section

--- fifty.js
+++ fifty.js
@@ -1,6 +1,6 @@
 function marquee() {
 	const title = 'FIFTY Music Festival — November 10–12 2021, Desert Valley';
-	const marqueeText = new Array(10).fill(title).join(' --- ');
+	const marqueeText = new Array(50).fill(title).join(' ——— ');
 	const marquee = document.querySelector('.marquee span');
 
 	marquee.innerHTML = marqueeText;
